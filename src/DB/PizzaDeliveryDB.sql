5a4e82f423e7291de6994516bf3eb8dfcd0ff3dd
Jari Näser <jari.naeser@samtrevano.ch>
2019-09-12 14:31:23 +0200

    Update PizzaDeliveryDB.sql

--- src/DB/PizzaDeliveryDB.sql
+++ src/DB/PizzaDeliveryDB.sql
@@ -1,55 +1,74 @@
-/* Codice SQL DataBase Progetto PizzaDelivery */
+/*
+	Codice SQL DataBase Progetto PizzaDelivery.
+
+	@author JariNaeser
+	@version 12.09.2019
+*/
 
 CREATE DATABASE PizzaDelivery;
 USE PizzaDelivery;
 
 CREATE TABLE TipoConsegna (
-	nome VARCHAR(20) PRIMARY KEY NOT NULL
+	nome VARCHAR(50) PRIMARY KEY NOT NULL
 );
 
 CREATE TABLE TipoUtente (
-	nome VARCHAR(20) PRIMARY KEY NOT NULL
+	nome VARCHAR(50) PRIMARY KEY NOT NULL
 );
 
 CREATE TABLE Utente(
 	username VARCHAR(50) PRIMARY KEY NOT NULL,
-    nome VARCHAR(30) NOT NULL,
-    cognome VARCHAR(20) NOT NULL,
-    via VARCHAR(20) NOT NULL,
-    cap VARCHAR(20) NOT NULL,
-    paese VARCHAR(20) NOT NULL,
-    email VARCHAR(50) NOT NULL,
+    nome VARCHAR(50) NOT NULL,
+    cognome VARCHAR(50) NOT NULL,
+    via VARCHAR(50) NOT NULL,
+    cap int(6) NOT NULL,
+    paese VARCHAR(50) NOT NULL,
+    email VARCHAR(200) NOT NULL,
     password VARCHAR(50) NOT NULL,
-    tipoUtente VARCHAR(20) NOT NULL,
+    tipoUtente VARCHAR(50) NOT NULL,
     FOREIGN KEY (tipoUtente) REFERENCES TipoUtente(nome)
 );
 
 CREATE TABLE Fattorino(
 	username VARCHAR(50) PRIMARY KEY NOT NULL,
-	posizioneLat VARCHAR(30) NOT NULL,
-    posizioneLon VARCHAR(30) NOT NULL,
-    inServizio BOOL NOT NULL,
+	posizioneLat VARCHAR(50) NOT NULL,
+    posizioneLon VARCHAR(50) NOT NULL,
+    inServizio TINYINT(1) NOT NULL,
     FOREIGN KEY (username) REFERENCES Utente(username)
 );
 
 CREATE TABLE Consegna (
-	id INT(6) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	id INT(6) PRIMARY KEY AUTO_INCREMENT,
     data DATETIME NOT NULL,
-    tipoConsegna VARCHAR(20) NOT NULL,
-    FOREIGN KEY (tipoConsegna) REFERENCES TipoConsegna(nome)
+    tipoConsegna VARCHAR(50) NOT NULL,
+    fattorino VARCHAR(50) NOT NULL,
+    FOREIGN KEY (tipoConsegna) REFERENCES TipoConsegna(nome),
+    FOREIGN KEY (fattorino) REFERENCES Fattorino(username)
 );
 
 CREATE TABLE Articolo (
-	nome VARCHAR(30) PRIMARY KEY NOT NULL,
-    descrizione VARCHAR(30) NOT NULL,
+	nome VARCHAR(50) PRIMARY KEY NOT NULL,
+    descrizione VARCHAR(50) NOT NULL,
     prezzo int(6) NOT NULL,
-    urlFoto VARCHAR(50)
+    urlFoto VARCHAR(250) DEFAULT "DefaultImagePath"
 );
 
 CREATE TABLE Ordinazione (
-	/* TO DO */
+	id INT(6) PRIMARY KEY AUTO_INCREMENT,
+    nomeCliente VARCHAR(50) NOT NULL,
+    cognomeCliente VARCHAR(50) NOT NULL,
+    numeroTelefonoCliente BIGINT(20) NOT NULL,			
+    via VARCHAR(50) NOT NULL,
+    cap INT(6) NOT NULL,
+    paese VARCHAR(50) NOT NULL,
+    data DATETIME NOT NULL
 );
 
-CREATE TABLE OrdineAtricolo (
-	/* TO DO */
+CREATE TABLE OrdineArticolo (
+	ordinazione INT(6) NOT NULL,
+    articolo VARCHAR(50) NOT NULL,
+	quantita INT(6) NOT NULL,
+    FOREIGN KEY (ordinazione) REFERENCES Ordinazione(id),
+    FOREIGN KEY (articolo) REFERENCES Articolo(nome),
+    PRIMARY KEY(ordinazione, articolo)
 );
