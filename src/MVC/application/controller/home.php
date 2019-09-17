0f39208b5a8e3501e17f74620b04dbcd910893ff
Jari Näser <jari.naeser@samtrevano.ch>
2019-09-17 15:11:03 +0200

    Update home.php

--- src/MVC/application/controller/home.php
+++ src/MVC/application/controller/home.php
@@ -6,7 +6,14 @@ class Home
     public function index()
     {
         //Istanzio model
-
+        if(file_exists('application/models/pizzadeliverymodel.php')){
+            require_once 'application/models/pizzadeliverymodel.php';
+            $pdModel = new PizzaDeliveryModel();
+            session_start();
+            $_SESSION['DATA'] = $pdModel->execQuery("SELECT * FROM Utente");
+        }else{
+            $_SESSION['DATA'] = "ERRORE nella classe home dei controller.";
+        }
 
         // Carico Views
         require 'application/views/_templates/header.php';
