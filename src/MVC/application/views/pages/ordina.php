2c9f43bd222decb431bf3e2b1953e09e5ae7f513
Jari Näser <jari.naeser@samtrevano.ch>
2019-09-19 16:16:08 +0200

    Update ordina.php

--- src/MVC/application/views/pages/ordina.php
+++ src/MVC/application/views/pages/ordina.php
@@ -1,1 +1,15 @@
-<h1>Ordina</h1>
+<div class="container">
+    <div class="col-md-12">
+        <h1>ORDINA</h1>
+        <div class="row">
+            <div class="col-md-4 align-self-start">
+                <input type="text" placeholder="Nome" name="nome" required>
+                <input type="text" placeholder="Cognome" name="cognome" required>
+                <input type="number" placeholder="Numero Telefono" name="numeroTelefono" required>
+            </div>
+            <div class="col-md-8 bg-secondary">
+                <!-- Google maps map selector -->
+            </div>
+        </div>
+    </div>
+</div>
