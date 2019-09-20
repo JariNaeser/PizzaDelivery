15c5db238ed2408d5642b040f60df8a985a8362a
Jari Näser <jari.naeser@samtrevano.ch>
2019-09-20 17:10:48 +0200

    Create errorDBConnection.php

---
+++ src/MVC/application/views/error/errorDBConnection.php
@@ -0,0 +1,5 @@
+<div class="col-md-12 text-center centerVerticallyDiv">
+    <h1 class="text-danger">ERRORE</h1>
+    <h3>Connessione con il Database inesistente.</h3>
+    <a href="<?php echo URL?>home/index" class="btn btn-danger btn-lg">Torna alla home</a>
+</div>
