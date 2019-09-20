d270a6dc180b5ecd1e979c3e5878631afe016019
Jari Näser <jari.naeser@samtrevano.ch>
2019-09-20 17:11:01 +0200

    Update footer.php

--- src/MVC/application/views/_templates/footer.php
+++ src/MVC/application/views/_templates/footer.php
@@ -1,8 +1,7 @@
 <footer class="page-footer font-small bg-light fixed-bottom">
-    <div class="footer-copyright text-center py-3">© 2019 Copyright:
-        <span> Jari Näser</span>
+    <div class="footer-copyright text-center py-3">
+        © 2019 Copyright:<span> Jari Näser</span>
     </div>
 </footer>
-
 </body>
 </html>
