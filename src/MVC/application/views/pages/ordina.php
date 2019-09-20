26d2a5de17c335aac53dc3659cfe797b1fca4ad1
Jari Näser <jari.naeser@samtrevano.ch>
2019-09-20 17:10:51 +0200

    Update ordina.php

--- src/MVC/application/views/pages/ordina.php
+++ src/MVC/application/views/pages/ordina.php
@@ -1,4 +1,4 @@
-<div class="container">
+<div class="container" style="padding-bottom: 70px">
     <div class="col-md-12">
         <h1>ORDINA</h1>
         <div class="row">
@@ -10,7 +10,49 @@
             <div class="col-md-8 bg-secondary">
                 <!-- Google maps map selector -->
                 <input type="text" placeholder="Via" name="via" required>
+                <input type="number" placeholder="Nr" name="numero" required>
             </div>
+        </div>
+        <div class="row">
+            <h3 style="margin-top: 1em">Seleziona Prodotti</h3>
+            <table class="table table-hover">
+                <thead>
+                <tr>
+                    <th scope="col">Immagine</th>
+                    <th scope="col">Articolo</th>
+                    <th scope="col">Descrizione</th>
+                    <th scope="col">Prezzo</th>
+                    <th scope="col">Aggiungi</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php $articoli = $_SESSION['articoli']; ?>
+                <?php $varMemorizzaCount = 0; ?>
+
+                <?php foreach ($articoli as $articolo): ?>
+                    <?php echo "<tr>" ?>
+
+                    <?php echo "<td><img style='height: 50px; width: 50px;' src='"
+                        . URL . $articolo['urlFoto'] . "'><td>" . $articolo['nome']
+                        . "<td>" . $articolo['descrizione'] . "<td>"
+                        . $articolo['prezzo'] . "</td><td>" .
+                        "<i class=\"fas fa-plus\"></i> " .
+                        "<i class=\"fas fa-minus\"></i> " .
+                        $varMemorizzaCount . "</td>"?>
+
+                    <?php echo "</tr>" ?>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+        <div class="row">
+            <ul class="list-group text-center">
+                <li class="list-group-item">Cras justo odio</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Morbi leo risus</li>
+                <li class="list-group-item">Porta ac consectetur ac</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
         </div>
     </div>
 </div>
