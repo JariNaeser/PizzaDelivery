b2f78a09c095fd6fada4e8797b6f50a71fd4cec4
Jari Näser <jari.naeser@samtrevano.ch>
2019-09-20 17:11:28 +0200

    Update home.php

--- src/MVC/application/controller/home.php
+++ src/MVC/application/controller/home.php
@@ -3,27 +3,44 @@
 class Home
 {
 
-    public function index()
+    private $pdModel;
+
+    public function __construct()
     {
-        //Istanzio model
         if(file_exists('application/models/pizzadeliverymodel.php')){
             require_once 'application/models/pizzadeliverymodel.php';
-            $pdModel = new PizzaDeliveryModel();
-            session_start();
-            $_SESSION['DATA'] = $pdModel->execQuery("SELECT * FROM utente;");
+            $this->pdModel = new PizzaDeliveryModel();
         }else{
-            $_SESSION['DATA'] = "ERRORE nella classe home dei controller.";
+            exit("ERRORE nel costruttore della classe home dei controller.");
         }
+    }
 
-        // Carico Views
-        require 'application/views/_templates/header.php';
-        require 'application/views/pages/benvenuto.php';
-        require 'application/views/_templates/footer.php';
+    public function index(){
+        if(isset($this->pdModel)){
+            $_SESSION['utenti'] = $this->pdModel->execQuery("SELECT * FROM utente;");
+            // Carico Views
+            require 'application/views/_templates/header.php';
+            require 'application/views/pages/benvenuto.php';
+            require 'application/views/_templates/footer.php';
+        }else{
+            $this->requireError();
+        }
     }
 
     public function ordina(){
+        if(isset($this->pdModel)){
+            $_SESSION['articoli'] = $this->pdModel->execQuery("SELECT * FROM articolo;");
+            require 'application/views/_templates/header.php';
+            require 'application/views/pages/ordina.php';
+            require 'application/views/_templates/footer.php';
+        }else{
+            $this->requireError();
+        }
+    }
+
+    public function requireError(){
         require 'application/views/_templates/header.php';
-        require 'application/views/pages/ordina.php';
+        require 'application/views/error/errorDBConnection.php';
         require 'application/views/_templates/footer.php';
     }
 
