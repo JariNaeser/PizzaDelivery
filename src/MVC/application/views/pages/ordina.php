209f1c4813743c91025901df1b6fa9da144ffb6d
Jari Näser <jari.naeser@samtrevano.ch>
2019-09-24 09:49:38 +0200

    Update ordina.php

--- src/MVC/application/views/pages/ordina.php
+++ src/MVC/application/views/pages/ordina.php
@@ -1,21 +1,35 @@
-<div class="container" style="padding-bottom: 70px">
-    <div class="col-md-12">
+<div class="container col-md-12" style="padding-bottom: 70px">
+
         <h1>ORDINA</h1>
         <div class="row">
-            <div class="col-md-4 align-self-start">
-                <input type="text" placeholder="Nome" name="nome" required>
-                <input type="text" placeholder="Cognome" name="cognome" required>
-                <input type="number" placeholder="Numero Telefono" name="numeroTelefono" required>
-            </div>
-            <div class="col-md-8 bg-secondary">
-                <!-- Google maps map selector -->
-                <input type="text" placeholder="Via" name="via" required>
-                <input type="number" placeholder="Nr" name="numero" required>
-            </div>
+            <form class="col-md-12">
+                <div class="form-row">
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="Nome" name="nome" required>
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="Cognome" name="cognome" required>
+                    </div>
+                    <div class="col-6">
+                        <input type="number" class="form-control" placeholder="Numero Telefono" name="numeroTelefono" required>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="col-6">
+                        <input type="text" class="form-control" placeholder="Via" name="via" required>
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="Nr." name="numero" required>
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="Paese" name="paese" required>
+                    </div>
+                </div>
+            </form>
         </div>
-        <div class="row">
+        <div class="row col-md-12">
             <h3 style="margin-top: 1em">Seleziona Prodotti</h3>
-            <div class="row table-responsive">
+            <div class="row table-responsive col-md-12">
                 <table class="table">
                     <thead>
                     <tr>
@@ -56,5 +70,5 @@
                 <li class="list-group-item">Vestibulum at eros</li>
             </ul>
         </div>
-    </div>
+
 </div>
