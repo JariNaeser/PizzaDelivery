80199d31b7e2eb2097f86efe999541f373d6aaac
Jari Näser <jari.naeser@samtrevano.ch>
2019-09-24 16:23:16 +0200

    Create admin.php

---
+++ src/MVC/application/views/_templates/headers/admin.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="author" content="Jari Naeser">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+    <!-- JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <!-- Bootstrap -->
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+    <!-- Font awesome -->
+    <script src="https://kit.fontawesome.com/8d7154f948.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" type="text/css" href="<?php echo URL ?>views/_templates/headers/style.css">
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+    <a class="navbar-brand" href="<?php echo URL?>home/index">PizzaDelivery</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="justify-content-end collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="<?php echo URL?>home/index">Home</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="<?php echo URL ?>home/ordina">Ordina</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="<?php echo URL ?>home/ordinazioni">Ordinazioni</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="<?php echo URL ?>home/consegne">Consegne</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="<?php echo URL ?>home/fattorini">Fattorini</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="<?php echo URL ?>home/gestionePizzeria">Gestione Pizzeria</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="<?php echo URL ?>home/logout">Logout</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
