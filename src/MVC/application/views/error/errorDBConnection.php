7a6839ea5d4308090bc61cc7a5282d33d6b797b2
Jari Näser <jari.naeser@samtrevano.ch>
2019-09-24 09:09:16 +0200

    Update errorDBConnection.php

--- src/MVC/application/views/error/errorDBConnection.php
+++ src/MVC/application/views/error/errorDBConnection.php
@@ -1,5 +1,6 @@
 <div class="col-md-12 text-center centerVerticallyDiv">
     <h1 class="text-danger">ERRORE</h1>
     <h3>Connessione con il Database inesistente.</h3>
-    <a href="<?php echo URL?>home/index" class="btn btn-danger btn-lg">Torna alla home</a>
+    <p><i>Contattare l'admin del sistema.</i></p>
+    <!-- <a href="<?php echo URL?>home/index" class="btn btn-danger btn-lg">Torna alla home</a> -->
 </div>
