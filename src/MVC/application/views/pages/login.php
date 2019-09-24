2cab185dd4bc051118f5a38c3448e817f20a4ec2
Jari Näser <jari.naeser@samtrevano.ch>
2019-09-24 16:22:49 +0200

    Update login.php

--- src/MVC/application/views/pages/login.php
+++ src/MVC/application/views/pages/login.php
@@ -1,1 +1,21 @@
-<h1>Login</h1>
+<div class="col-md-12">
+    <br>
+    <h1 class="text-center">Login</h1>
+    <br>
+    <br>
+    <br>
+    <div class="row">
+        <div class="col-md-4"></div>
+        <form class="text-center col-md-4" action="<?php echo URL ?>home/login" method="post">
+            <div class="form-group col-md-12">
+                <input type="text" class="form-control" placeholder="Username" name="username" required>
+            </div>
+            <div class="form-group col-md-12">
+                <input type="password" class="form-control" placeholder="Password" name="password" required>
+            </div>
+            <div class="form-group col-md-12">
+                <button type="submit" class="btn btn-danger btn-lg col-md-12">Login</button>
+            </div>
+        </form>
+    </div>
+</div>
