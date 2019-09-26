79bc0ddc1d18eedcecfdc1da83c5907e77b0270e
Jari Näser <jari.naeser@samtrevano.ch>
2019-09-26 16:12:02 +0200

    Update benvenuto.php

--- src/MVC/application/views/pages/benvenuto.php
+++ src/MVC/application/views/pages/benvenuto.php
@@ -1,10 +1,8 @@
-<div class="col-md-12 text-center centerVerticallyDiv">
+<div class="col-md-12 text-center">
     <h1>Benvenuto da <br> <span class="text-danger">PizzaDelivery</span></h1>
     <p>Hai fame? Ecco la soluzione per te, ordina e ricevi in <br> breve tempo la tua pizza preferita.</p>
     <a href="<?php echo URL?>home/ordina" class="btn btn-danger btn-lg">Ordina ora</a>
 </div>
-
-
 
 
 
