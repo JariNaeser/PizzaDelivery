6552ebad190abe152e95073dae042a5f32da6a2e
Jari Näser <jari.naeser@samtrevano.ch>
2019-09-26 16:12:00 +0200

    Create confermaOrdine.php

---
+++ src/MVC/application/views/pages/confermaOrdine.php
@@ -0,0 +1,28 @@
+<h4>Informazioni personali</h4>
+<div class="row">
+    <div class="form-row">
+        <div class="col">
+            <input type="text" class="form-control" placeholder="Nome" name="nome" required>
+        </div>
+        <div class="col">
+            <input type="text" class="form-control" placeholder="Cognome" name="cognome" required>
+        </div>
+        <div class="col">
+            <input type="text" class="form-control" placeholder="Paese" name="paese" required>
+        </div>
+    </div>
+    <div class="form-row">
+        <div class="col-5">
+            <input type="text" class="form-control" placeholder="Via" name="via" required>
+        </div>
+        <div class="col">
+            <input type="number" class="form-control" placeholder="Nr." name="numero" required>
+        </div>
+        <div class="col-5">
+            <input type="number" class="form-control" placeholder="Nr. Telefono" name="numeroTelefono" required>
+        </div>
+    </div>
+</div>
+
+Sviluppare pagina conferma ordine dove si mettono tutti i propri dati con magari mappa google maps e selezionare quante pizze si vuole
+ordinare di ogni tipo selezionato precedentemente.
