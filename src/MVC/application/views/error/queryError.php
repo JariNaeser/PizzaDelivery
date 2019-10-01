1a9751a5e04f8120b2e6c9e399370ffb10525331
Jari Näser <jari.naeser@samtrevano.ch>
2019-10-01 16:13:48 +0200

    Create queryError.php

---
+++ src/MVC/application/views/error/queryError.php
@@ -0,0 +1,4 @@
+<div class="container text-center" style="padding: 1em">
+    <h1 class="text-danger">C'è stato un problema nell'esecuzione di una query.</h1>
+    <a href="<?php echo URL?>home/index" class="btn btn-danger btn-lg">Torna alla home</a>
+</div>
