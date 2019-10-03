7b8d85f8a90fbc398ed74ebe9c816bda799ff4dc
Jari Näser <jari.naeser@samtrevano.ch>
2019-10-03 15:26:37 +0200

    Update ordinazioni.php

--- src/MVC/application/views/pages/ordinazioni.php
+++ src/MVC/application/views/pages/ordinazioni.php
@@ -1,1 +1,22 @@
-<h1>Ordinazioni</h1>
+<div class="container text-center">
+    <h1>ORDINAZIONI</h1>
+    <div class="row col-md-12 table-responsive">
+        <table class="table table-bordered">
+            <thead>
+            <tr>
+                <th scope="col">Immagine</th>
+                <th scope="col">Articolo</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Quantità</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php if(isset($_SESSION['ordinazioni']) && count($_SESSION['ordinazioni']) > 0): ?>
+
+            <?php else: ?>
+                <?php echo "<tr><td colspan='4'>Nessuna ordinazione trovata.</td></tr>"?>
+            <?php endif; ?>
+            </tbody>
+        </table>
+    </div>
+</div>
