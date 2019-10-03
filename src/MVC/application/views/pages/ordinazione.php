a8556bbf092658e2d688e85d937b6926a6fd4f63
Jari Näser <jari.naeser@samtrevano.ch>
2019-10-03 16:27:59 +0200

    Update ordinazione.php

--- src/MVC/application/views/pages/ordinazione.php
+++ src/MVC/application/views/pages/ordinazione.php
@@ -1,1 +1,11 @@
-<h1>Ordinazione</h1>
+<div class="container padding-footer text-center">
+    <?php $ordine = $_SESSION['ordine']; ?>
+    <h1>ORDINAZIONE #<?php echo $ordine[0][0]['id'] ?></h1>
+    <pre>
+        <?php print_r($ordine); ?>
+    </pre>
+
+    <h1 class="text-danger">METTERE A POSTO import file CSS (solo in questa classe).</h1>
+
+    <a href="<?php echo URL?>home/ordinazioni" class="btn btn-danger btn-lg">Torna agli ordini</a>
+</div>
