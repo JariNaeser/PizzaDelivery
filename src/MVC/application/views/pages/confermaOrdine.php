9f2abbbbe4d1278bd652eea8a26c0388a9f11358
Jari Näser <jari.naeser@samtrevano.ch>
2019-10-03 15:26:42 +0200

    Update confermaOrdine.php

--- src/MVC/application/views/pages/confermaOrdine.php
+++ src/MVC/application/views/pages/confermaOrdine.php
@@ -1,70 +1,68 @@
 <script>const prezzoFinale = new Array(<?php echo count($_SESSION['cart']); ?>);</script>
-<h1 class="text-center">CONFERMA ORDINE</h1>
-<form action="<?php echo URL . "home/creaOrdine"?>" method="post" class="container" style="padding: 0px; padding: 1em; padding-bottom: 70px;">
-    <h4>Informazioni personali</h4>
-    <div class="form-row">
-        <div class="col">
-            <input type="text" class="form-control" placeholder="Nome" name="nome" required>
+<div class="container padding-footer">
+    <form action="<?php echo URL . "home/creaOrdine"?>" method="post" style="padding: 0px;">
+        <h1 class="text-center">CONFERMA ORDINE</h1>
+        <h4>Informazioni personali</h4>
+        <div class="form-row">
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Nome" name="nome" required>
+            </div>
+            <div class="col inner-addon right-addon">
+                <input type="text" class="form-control" placeholder="Cognome" name="cognome" required>
+            </div>
         </div>
-        <div class="col inner-addon right-addon">
-            <input type="text" class="form-control" placeholder="Cognome" name="cognome" required>
+        <br>
+        <div class="form-row">
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Paese" name="paese" required>
+            </div>
+            <div class="col">
+                <input type="number" class="form-control" placeholder="Nr. Telefono" name="numeroTelefono" required>
+            </div>
         </div>
-    </div>
-    <br>
-    <div class="form-row">
-        <div class="col">
-            <input type="text" class="form-control" placeholder="Paese" name="paese" required>
+        <br>
+        <div class="form-row">
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Via" name="via" required>
+            </div>
+            <div class="col-3">
+                <input type="number" class="form-control" placeholder="Cap" name="cap" required>
+            </div>
+            <div class="col-3">
+                <input type="text" class="form-control" placeholder="Nr." name="numero" required>
+            </div>
         </div>
-        <div class="col">
-            <input type="number" class="form-control" placeholder="Nr. Telefono" name="numeroTelefono" required>
-        </div>
-    </div>
-    <br>
-    <div class="form-row">
-        <div class="col">
-            <input type="text" class="form-control" placeholder="Via" name="via" required>
-        </div>
-        <div class="col-3">
-            <input type="number" class="form-control" placeholder="Cap" name="cap" required>
-        </div>
-        <div class="col-3">
-            <input type="number" class="form-control" placeholder="Nr." name="numero" required>
-        </div>
-    </div>
-    <br>
-    <h4>Prodotti selezionati</h4>
-    <div class="row col-md-12 table-responsive">
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th scope="col">Immagine</th>
-                    <th scope="col">Articolo</th>
-                    <th scope="col">Descrizione</th>
-                    <th scope="col">Quantità</th>
-                    <th scope="col">Prezzo</th>
-                    <th scope="col">Prezzo finale</th>
-                </tr>
-            </thead>
-            <tbody>
-            <?php $cart = $_SESSION['cart']; ?>
-            <?php if(count($cart) == 0): ?>
-                <?php echo "<tr><td colspan='5' class='text-center'>Nessun elemento selezionato.</td></tr>"; ?>
-            <?php else: ?>
-                <?php if(count($cart) > 0): ?>
-                    <?php foreach ($cart as $element): ?>
+        <br>
+        <h4>Prodotti selezionati</h4>
+        <div class="row col-md-12 table-responsive">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col">Immagine</th>
+                        <th scope="col">Articolo</th>
+                        <th scope="col">Descrizione</th>
+                        <th scope="col">Quantità</th>
+                        <th scope="col">Prezzo</th>
+                        <th scope="col">Prezzo finale</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php if(isset($_SESSION['cart']) && count($_SESSION['cart']) > 0): ?>
+                    <?php $cart = $_SESSION['cart']; ?>
+                    <?php if(count($cart) > 0): ?>
+                        <?php foreach ($cart as $element): ?>
 
-                        <?php $select = "<select class='form-control form-control-sm' id='select" . $element[0]['id'] . "'>";
-                                        for($i = 0; $i < 31; $i++){if($i == 1){$select .= "<option selected='selected'>$i</option>";}else{$select .= "<option>$i</option>";}}
-                                        $select .= "</select>" ?>
+                            <?php $select = "<select class='form-control form-control-sm' name='select" . $element[0]['id'] . "' id='select" . $element[0]['id'] . "'>";
+                                                for($i = 0; $i < 31; $i++){if($i == 1){$select .= "<option selected='selected'>$i</option>";}else{$select .= "<option>$i</option>";}}
+                                                $select .= "</select>" ?>
 
-                        <?php echo "<tr id='" . $element[0]['id'] . "'><td><img style='height: 50px; width: 50px;' src='" . URL . $element[0]['urlFoto']
-                            . "'></td><td>" . $element[0]['nome'] . "</td><td>"
-                            . $element[0]['descrizione'] . "</td><td>"
-                            . $select . "</td><td>"
-                            . $element[0]['prezzo'] . "</td><td id='total" . $element[0]['id'] . "'>x</td></tr>";?>
+                            <?php echo "<tr id='" . $element[0]['id'] . "'><td><img style='height: 50px; width: 50px;' src='" . URL . $element[0]['urlFoto']
+                                    . "'></td><td>" . $element[0]['nome'] . "</td><td>"
+                                    . $element[0]['descrizione'] . "</td><td>"
+                                    . $select . "</td><td>"
+                                    . $element[0]['prezzo'] . "</td><td id='total" . $element[0]['id'] . "'>x</td></tr>";?>
 
                             <script>
-
                                 function countTotal() {
                                     var total = 0;
                                     for(var i = 0; i < prezzoFinale.length; i++){
@@ -93,169 +91,171 @@
 
                             </script>
 
-                    <?php endforeach; ?>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                <?php else: ?>
+                    <?php echo "<tr><td colspan='6' class='text-center'>Nessun elemento selezionato.</td></tr>"; ?>
                 <?php endif; ?>
-            <?php endif; ?>
-
-            </tbody>
-        </table>
-    </div>
-    <h3 class="text-danger">Costo Totale: <span id="totalPrice"></span>.-</h3>
-    <script>
-        countTotal();
-    </script>
-    <div class="col-md-12 text-center">
-        <button type="button" class="btn btn-danger btn-lg" id="ordina">Ordina</button>
+                </tbody>
+            </table>
+        </div>
+        <h3 class="text-danger">Costo Totale: <span id="totalPrice"></span>.-</h3>
         <script>
+            countTotal();
+        </script>
+        <div class="col-md-12 text-center">
+            <button type="button" class="btn btn-danger btn-lg" id="ordina">Ordina</button>
+            <script>
 
-            const status = [false, false, false, false, false, false, false];
+                const status = [false, false, false, false, false, false, false];
 
-            //Si riferisce alla lunghezza massima della tabella del DB.
-            const LUNGHEZZA_MASSIMA_NOME = 50;
-            const LUNGHEZZA_MASSIMA_COGNOME = 50;
-            const LUNGHEZZA_MASSIMA_TELEFONO = 20;
-            const LUNGHEZZA_MASSIMA_VIA = 44;       //Con aggiunta di cap, 50 - 5 - 1(spazio) = 44
-            const LUNGHEZZA_MASSIMA_PAESE = 50;
-            const LUNGHEZZA_MASSIMA_CAP = 6;
-            const LUNGHEZZA_MASSIMA_NUMERO = 5;
+                //Si riferisce alla lunghezza massima della tabella del DB.
+                const LUNGHEZZA_MASSIMA_NOME = 50;
+                const LUNGHEZZA_MASSIMA_COGNOME = 50;
+                const LUNGHEZZA_MASSIMA_TELEFONO = 20;
+                const LUNGHEZZA_MASSIMA_VIA = 44;       //Con aggiunta di cap, 50 - 5 - 1(spazio) = 44
+                const LUNGHEZZA_MASSIMA_PAESE = 50;
+                const LUNGHEZZA_MASSIMA_CAP = 6;
+                const LUNGHEZZA_MASSIMA_NUMERO = 5;
 
-            function validate(string, maxLen, regex){
-                try{
-                    string = string.trim();
-                    if(string.length > 0 && string.length <= maxLen){
-                        if(!regex.test(string)){
-                            return true;
+                function validate(string, maxLen, regex){
+                    try{
+                        string = string.trim();
+                        if(string.length > 0 && string.length <= maxLen){
+                            if(!regex.test(string)){
+                                return true;
+                            }
                         }
-                    }
-                    return false;
-                }catch(error){
-                    console.log("Error: " + error);
-                    return false;
-                }
-            }
-
-            // nome field
-            var nameSelector = $('input[name=nome]');
-            nameSelector.blur(function(event){
-                if(validate(nameSelector.val(), LUNGHEZZA_MASSIMA_NOME, /([^A-Za-zöäüÖÄÜàèìòùÀÈÌÒÙÉé -.])/)){
-                    isOk(nameSelector);
-                    status[0] = true;
-                }else{
-                    isNotOk(nameSelector);
-                    status[0] = false;
-                }
-                checkIfOk();
-            });
-
-            // cognome field
-            var surnameSelector = $('input[name=cognome]');
-            surnameSelector.blur(function(event){
-                if(validate(surnameSelector.val(), LUNGHEZZA_MASSIMA_COGNOME, /([^A-Za-zöäüÖÄÜàèìòùÀÈÌÒÙÉé -.])/)){
-                    isOk(surnameSelector);
-                    status[1] = true;
-                }else{
-                    isNotOk(surnameSelector);
-                    status[1] = false;
-                }
-                checkIfOk();
-            });
-
-            // paese field
-            var paeseSelector = $('input[name=paese]');
-            paeseSelector.blur(function(event){
-                if(validate(paeseSelector.val(), LUNGHEZZA_MASSIMA_PAESE, /([^A-Za-zöäüÖÄÜàèìòùÀÈÌÒÙÉé -])/)){
-                    isOk(paeseSelector);
-                    status[2] = true;
-                }else{
-                    isNotOk(paeseSelector);
-                    status[2] = false;
-                }
-                checkIfOk();
-            });
-
-            // telefono field
-            var telefonoSelector = $('input[name=numeroTelefono]', );
-            telefonoSelector.blur(function(event){
-                if(validate(telefonoSelector.val(), LUNGHEZZA_MASSIMA_TELEFONO, /([^0-9+ ])/)){
-                    isOk(telefonoSelector);
-                    status[3] = true;
-                }else{
-                    isNotOk(telefonoSelector);
-                    status[3] = false;
-                }
-                checkIfOk();
-            });
-
-            // via field
-            var viaSelector = $('input[name=via]');
-            viaSelector.blur(function(event){
-                if(validate(viaSelector.val(), LUNGHEZZA_MASSIMA_VIA, /([^A-Za-zöäüÖÄÜàèìòùÀÈÌÒÙÉé -.])/)){
-                    isOk(viaSelector);
-                    status[4] = true;
-                }else{
-                    isNotOk(viaSelector);
-                    status[4] = false;
-                }
-                checkIfOk();
-            });
-
-            // cap field
-            var capSelector = $('input[name=cap]');
-            capSelector.blur(function(event){
-                if(validate(capSelector.val(), LUNGHEZZA_MASSIMA_CAP, /([^0-9])/)){
-                    isOk(capSelector);
-                    status[5] = true;
-                }else{
-                    isNotOk(capSelector);
-                    status[5] = false;
-                }
-                checkIfOk();
-            });
-
-            // numero field
-            var numeroSelector = $('input[name=numero]');
-            numeroSelector.blur(function(event){
-                if(validate(numeroSelector.val(), LUNGHEZZA_MASSIMA_NUMERO, /([^0-9A-za-z])/)){
-                    isOk(numeroSelector);
-                    status[6] = true;
-                }else{
-                    isNotOk(numeroSelector);
-                    status[6] = false;
-                }
-                checkIfOk();
-            });
-
-            function isOk(selector){
-                selector.css('border-color', '#abdd92');
-            }
-
-            function isNotOk(selector){
-                selector.css('border-color', '#f76a6a');
-            }
-
-            function areAllOk(){
-                for(var i = 0; i < status.length; i++){
-                    if(!status[i]){
+                        return false;
+                    }catch(error){
+                        console.log("Error: " + error);
                         return false;
                     }
                 }
-                return true;
-            }
 
-            function checkIfOk(){
-                if(areAllOk()){
-                    $('#ordina').removeAttr('type').attr('type', 'submit');
-                }else{
-                    $('#ordina').removeAttr('type').attr('type', 'button');
+                // nome field
+                var nameSelector = $('input[name=nome]');
+                nameSelector.blur(function(event){
+                    if(validate(nameSelector.val(), LUNGHEZZA_MASSIMA_NOME, /([^A-Za-zöäüÖÄÜàèìòùÀÈÌÒÙÉé -.])/)){
+                        isOk(nameSelector);
+                        status[0] = true;
+                    }else{
+                        isNotOk(nameSelector);
+                        status[0] = false;
+                    }
+                    checkIfOk();
+                });
+
+                // cognome field
+                var surnameSelector = $('input[name=cognome]');
+                surnameSelector.blur(function(event){
+                    if(validate(surnameSelector.val(), LUNGHEZZA_MASSIMA_COGNOME, /([^A-Za-zöäüÖÄÜàèìòùÀÈÌÒÙÉé -.])/)){
+                        isOk(surnameSelector);
+                        status[1] = true;
+                    }else{
+                        isNotOk(surnameSelector);
+                        status[1] = false;
+                    }
+                    checkIfOk();
+                });
+
+                // paese field
+                var paeseSelector = $('input[name=paese]');
+                paeseSelector.blur(function(event){
+                    if(validate(paeseSelector.val(), LUNGHEZZA_MASSIMA_PAESE, /([^A-Za-zöäüÖÄÜàèìòùÀÈÌÒÙÉé -])/)){
+                        isOk(paeseSelector);
+                        status[2] = true;
+                    }else{
+                        isNotOk(paeseSelector);
+                        status[2] = false;
+                    }
+                    checkIfOk();
+                });
+
+                // telefono field
+                var telefonoSelector = $('input[name=numeroTelefono]', );
+                telefonoSelector.blur(function(event){
+                    if(validate(telefonoSelector.val(), LUNGHEZZA_MASSIMA_TELEFONO, /([^0-9+ ])/)){
+                        isOk(telefonoSelector);
+                        status[3] = true;
+                    }else{
+                        isNotOk(telefonoSelector);
+                        status[3] = false;
+                    }
+                    checkIfOk();
+                });
+
+                // via field
+                var viaSelector = $('input[name=via]');
+                viaSelector.blur(function(event){
+                    if(validate(viaSelector.val(), LUNGHEZZA_MASSIMA_VIA, /([^A-Za-zöäüÖÄÜàèìòùÀÈÌÒÙÉé -.])/)){
+                        isOk(viaSelector);
+                        status[4] = true;
+                    }else{
+                        isNotOk(viaSelector);
+                        status[4] = false;
+                    }
+                    checkIfOk();
+                });
+
+                // cap field
+                var capSelector = $('input[name=cap]');
+                capSelector.blur(function(event){
+                    if(validate(capSelector.val(), LUNGHEZZA_MASSIMA_CAP, /([^0-9])/)){
+                        isOk(capSelector);
+                        status[5] = true;
+                    }else{
+                        isNotOk(capSelector);
+                        status[5] = false;
+                    }
+                    checkIfOk();
+                });
+
+                // numero field
+                var numeroSelector = $('input[name=numero]');
+                numeroSelector.blur(function(event){
+                    if(validate(numeroSelector.val(), LUNGHEZZA_MASSIMA_NUMERO, /([^0-9A-za-z])/)){
+                        isOk(numeroSelector);
+                        status[6] = true;
+                    }else{
+                        isNotOk(numeroSelector);
+                        status[6] = false;
+                    }
+                    checkIfOk();
+                });
+
+                function isOk(selector){
+                    selector.css('border-color', '#abdd92');
                 }
-            }
 
-            $('#ordina').click(function(){
-               if($('#ordina').attr('type') === 'button'){
-                   alert("Devi immettere correttamente tutte le tue informazioni personali.");
-               }
-            });
+                function isNotOk(selector){
+                    selector.css('border-color', '#f76a6a');
+                }
 
-        </script>
-    </div>
-</form>
+                function areAllOk(){
+                    for(var i = 0; i < status.length; i++){
+                        if(!status[i]){
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+
+                function checkIfOk(){
+                    if(areAllOk()){
+                        $('#ordina').removeAttr('type').attr('type', 'submit');
+                    }else{
+                        $('#ordina').removeAttr('type').attr('type', 'button');
+                    }
+                }
+
+                $('#ordina').click(function(){
+                   if($('#ordina').attr('type') === 'button'){
+                       alert("Devi immettere correttamente tutte le tue informazioni personali.");
+                   }
+                });
+
+            </script>
+        </div>
+    </form>
+</div>
