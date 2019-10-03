3839986181bf740754b871446078c24544812aa6
Jari Näser <jari.naeser@samtrevano.ch>
2019-10-03 15:26:35 +0200

    Update ringraziamentoOrdine.php

--- src/MVC/application/views/pages/ringraziamentoOrdine.php
+++ src/MVC/application/views/pages/ringraziamentoOrdine.php
@@ -1,6 +1,6 @@
 <div class="container text-center" style="padding: 1em;">
     <h1>Il tuo ordine è stato inviato.</h1>
-    <p>Quando il fattorino sarà nelle sue vicinanze la avviserà via chiamata sul cellulare.</p>
+    <p>Quando il fattorino sarà nelle tue vicinanze ti avviserà tramite chiamata sul cellulare.</p>
     <p><i>I pagamenti verranno effettuati sul posto.</i></p>
     <a href="<?php echo URL?>home/index" class="btn btn-danger btn-lg">Torna alla home</a>
 </div>
