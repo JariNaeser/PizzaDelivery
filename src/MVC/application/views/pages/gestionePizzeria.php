2969a8d0923adb68969ca9d7ee4fd0572353c0b0
Jari Näser <jari.naeser@samtrevano.ch>
2019-10-04 15:40:51 +0200

    Update gestionePizzeria.php

--- src/MVC/application/views/pages/gestionePizzeria.php
+++ src/MVC/application/views/pages/gestionePizzeria.php
@@ -1,2 +1,16 @@
-<h1>Gestione Utenti</h1>
-<h1>Gestione Articoli</h1>
+<div class="container">
+    <h1 class="text-center">GESTIONE</h1>
+    <br>
+    <h3>Gestisci Utenti</h3>
+    <hr>
+    <form class="text-center">
+        <div class="row">
+            <input type="email" class="form-control col-4" placeholder="Cerca Utente">
+            <button class="btn btn-danger col-4"><i class="fas fa-plus"></i> Nuovo Utente</button>
+        </div>
+    </form>
+    <br>
+    <h3>Gestisci Articoli</h3>
+</div>
+
+
