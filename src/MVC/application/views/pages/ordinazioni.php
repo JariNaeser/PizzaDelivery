a7618cfec19dd4759f798f02b6a7f70ecf9cf0fb
Jari Näser <jari.naeser@samtrevano.ch>
2019-10-04 14:07:00 +0200

    Update ordinazioni.php

--- src/MVC/application/views/pages/ordinazioni.php
+++ src/MVC/application/views/pages/ordinazioni.php
@@ -4,17 +4,21 @@
         <table class="table table-bordered">
             <thead>
             <tr>
-                <th scope="col">Immagine</th>
-                <th scope="col">Articolo</th>
-                <th scope="col">Descrizione</th>
-                <th scope="col">Quantità</th>
+                <th scope="col">ID</th>
+                <th scope="col">Cliente</th>
+                <th scope="col">Posizione</th>
+                <th scope="col">Pz. Ordine</th>
             </tr>
             </thead>
             <tbody>
             <?php if(isset($_SESSION['ordinazioni']) && count($_SESSION['ordinazioni']) > 0): ?>
                 <?php $ordini = $_SESSION['ordinazioni']; ?>
                 <?php foreach ($ordini as $ordine): ?>
-                    <?php echo "<tr class='clickable-row' data-href='" . URL . "home/ordinazione/" . $ordine[0]['id'] . "'><td>" . $ordine[0]['id'] . "</td><td>" . $ordine[0]['nomeCliente'] . " " . $ordine[0]['cognomeCliente'] . "</td><td>" . $ordine[0]['via'] . "</td><td>" . $ordine[1]['quantita'] . "</td></tr>" ?>
+                    <?php $sum = 0; ?>
+                    <?php for($i = 1; $i < count($ordine); $i++): ?>
+                        <?php $sum += $ordine[$i]['quantita']; ?>
+                    <?php endfor; ?>
+                    <?php echo "<tr class='clickable-row' data-href='" . URL . "home/ordinazione/" . $ordine[0]['id'] . "'><td>" . $ordine[0]['id'] . "</td><td>" . $ordine[0]['nomeCliente'] . " " . $ordine[0]['cognomeCliente'] . "</td><td>" . $ordine[0]['via'] . "</td><td>" . $sum . "</td></tr>" ?>
                 <?php endforeach; ?>
             <?php else: ?>
                 <?php echo "<tr><td colspan='4'>Nessuna ordinazione trovata.</td></tr>"?>
