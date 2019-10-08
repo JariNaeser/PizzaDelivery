3648b34aafe7386e78cca484e39e8988c74ccb9f
Jari Näser <jari.naeser@samtrevano.ch>
2019-10-08 14:26:02 +0200

    Update queryError.php

--- src/MVC/application/views/error/queryError.php
+++ src/MVC/application/views/error/queryError.php
@@ -1,4 +1,7 @@
 <div class="container text-center" style="padding: 1em">
     <h1 class="text-danger">C'è stato un problema nell'esecuzione di una query.</h1>
+    <br>
+    <h3>Error:</h3>
+    <p><?php if(isset($_SESSION['queryError'])){echo $_SESSION['queryError'];} ?></p>
     <a href="<?php echo URL?>home/index" class="btn btn-danger btn-lg">Torna alla home</a>
 </div>
