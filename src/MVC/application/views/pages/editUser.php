c07c1f1a6296c1cc5d5d7611e0405b10e06f8ca7
Jari Näser <jari.naeser@samtrevano.ch>
2019-10-08 14:26:07 +0200

    Update editUser.php

--- src/MVC/application/views/pages/editUser.php
+++ src/MVC/application/views/pages/editUser.php
@@ -3,50 +3,59 @@
     <div class="col-md-12 container text-center padding-footer">
         <h1>Utente <?php echo $userToModify['username']; ?></h1>
         <br>
-        <form>
+        <form action="<?php echo URL . "home/modifyUserContent"?>" method="post">
             <div class="table-responsive col-md-6 mx-auto">
                 <table class="table table-striped">
                     <tbody>
                         <tr class="text-inline">
-                            <td scope="col">Username</td>
-                            <td><input type="text" class="form-control" value="<?php echo $userToModify['username']; ?>"></td>
-                        </tr>
-                        <tr class="text-inline">
                             <td scope="col">Nome</td>
-                            <td><input type="text" class="form-control" value="<?php echo $userToModify['nome']; ?>"></td>
+                            <td><input type="text" class="form-control" value="<?php echo $userToModify['nome']; ?>" name="nomeMU" required></td>
                         </tr>
                         <tr class="text-inline">
                             <td scope="col">Cognome</td>
-                            <td><input type="text" class="form-control" value="<?php echo $userToModify['cognome']; ?>"></td>
+                            <td><input type="text" class="form-control" value="<?php echo $userToModify['cognome']; ?>" name="cognomeMU" required></td>
                         </tr>
                         <tr class="text-inline">
                             <td scope="col">Via</td>
-                            <td><input type="text" class="form-control" value="<?php echo $userToModify['via']; ?>"></td>
+                            <td><input type="text" class="form-control" value="<?php echo $userToModify['via']; ?>" name="viaMU" required></td>
                         </tr>
                         <tr class="text-inline">
                             <td scope="col">CAP</td>
-                            <td><input type="number" class="form-control" value="<?php echo $userToModify['cap']; ?>"></td>
+                            <td><input type="number" class="form-control" value="<?php echo $userToModify['cap']; ?>" name="capMU" required></td>
                         </tr>
                         <tr class="text-inline">
-                            <td scope="col">Paeser</td>
-                            <td><input type="text" class="form-control" value="<?php echo $userToModify['paese']; ?>"></td>
+                            <td scope="col">Paese</td>
+                            <td><input type="text" class="form-control" value="<?php echo $userToModify['paese']; ?>" name="paeseMU" required></td>
                         </tr>
                         <tr class="text-inline">
                             <td scope="col">E-Mail</td>
-                            <td><input type="email" class="form-control" value="<?php echo $userToModify['email']; ?>"></td>
+                            <td><input type="email" class="form-control" value="<?php echo $userToModify['email']; ?>" name="emailMU" required></td>
                         </tr>
                         <tr class="text-inline">
                             <td scope="col">Password</td>
-                            <td><button class="btn btn-primary btn-sm">Modifica Password</button></td>
+                            <td><input type="email" class="form-control" placeholder="Password non mostrata." name="emailMU" required></td>
                         </tr>
                         <tr class="text-inline">
                             <td scope="col">Tipologia</td>
-                            <td><input type="text" class="form-control" value="<?php echo $userToModify['tipoUtente']; ?>"></td>
+                            <td>
+                                <select class="form-control" name="tipologiaMU" required>
+                                    <?php if(isset($_SESSION['userTypes'])): ?>
+                                        <?php foreach($_SESSION['userTypes'] as $type): ?>
+                                            <?php if(strcmp($type['nome'], $userToModify['tipoUtente']) == 0): ?>
+                                                <?php echo "<option selected='selected'>" . $type['nome'] . "</option>"; ?>
+                                            <?php else: ?>
+                                                <?php echo "<option>" . $type['nome'] . "</option>"; ?>
+                                            <?php endif; ?>
+                                        <?php endforeach; ?>
+                                    <?php endif; ?>
+                                </select>
+                            </td>
                         </tr>
                     </tbody>
                 </table>
             </div>
-            <button type="submit" class="btn btn-danger btn-lg">Aggiorna</button>
+            <a href="<?php echo URL . "home/eliminaUtente/" . $userToModify['username'];?>" class="btn btn-danger btn-lg">Elimina</a>
+            <button type="submit" class="btn btn-primary btn-lg">Aggiorna</button>
             <a href="<?php echo URL . "home/gestionePizzeria";?>" class="btn btn-danger btn-lg">Esci</a>
         </form>
 
