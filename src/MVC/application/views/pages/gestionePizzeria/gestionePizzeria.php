42bcf52c8fa29748f65bbb25f77b80bdca09f4a3
Jari Näser <jari.naeser@samtrevano.ch>
2019-10-10 14:05:29 +0200

    Update gestionePizzeria.php

--- src/MVC/application/views/pages/gestionePizzeria/gestionePizzeria.php
+++ src/MVC/application/views/pages/gestionePizzeria/gestionePizzeria.php
@@ -1,4 +1,4 @@
-<div class="container">
+<div class="container padding-footer">
     <h1 class="text-center">GESTIONE</h1>
     <br>
     <h3>Gestisci Utenti</h3>
@@ -8,8 +8,8 @@
             <div class="row">
                 <div class="col-md-6">
                     <div class="row">
-                        <input type="email" class="form-control col-md-8" placeholder="Cerca Utente">
-                        <button class="btn btn-danger col-sm-3"><i class="fas fa-search"></i> Cerca</button>
+                        <input type="text" class="form-control col-md-8" placeholder="Cerca Utente">
+                        <button class="btn btn-danger col-sm-3" type="submit"><i class="fas fa-search"></i> Cerca</button>
                     </div>
                 </div>
                 <div class="col-sm-6">
@@ -45,6 +45,49 @@
     </form>
     <br>
     <h3>Gestisci Articoli</h3>
+    <hr>
+    <form class="text-center">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="row">
+                        <input type="text" class="form-control col-md-8" placeholder="Cerca Articolo">
+                        <button class="btn btn-danger col-sm-3" type="submit"><i class="fas fa-search"></i> Cerca</button>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <a href="<?php echo URL?>gestionePizzeria/creaArticolo" class="btn btn-danger btn-md col-sm-12"><i class="fas fa-plus"></i> Aggiungi Articolo</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col">Immagine</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Prezzo</th>
+                <th scope="col">Modifica</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php if(isset($_SESSION['prodotti']) && count($_SESSION['prodotti']) > 0): ?>
+                <?php $prodotti = $_SESSION['prodotti']; ?>
+                <?php foreach ($prodotti as $prodotto): ?>
+                    <?php echo "<tr><td><img style='height: 50px; width: 50px;' src='" . URL . $prodotto['urlFoto']
+                        . "'></td><td>" . $prodotto['nome'] . "</td><td>" . $prodotto['prezzo'] . "</td><td><a href='" . URL . "gestionePizzeria/modifyArticolo/" . $prodotto['id'] . "' class='text-dark'><i class=\"fas fa-user-edit\"></i></a></td></tr>" ?>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <?php echo "<tr><td colspan='4'>Nessun prodotto trovato nel DB.</td>"; ?>
+            <?php endif; ?>
+            </tbody>
+        </table>
+
+    </form>
 </div>
 
 
