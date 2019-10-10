eed914940f03b10c598f54870dd5418d183ae3d3
Jari Näser <jari.naeser@samtrevano.ch>
2019-10-10 15:16:55 +0200

    Update gestionePizzeria.php

--- src/MVC/application/views/pages/gestionePizzeria/gestionePizzeria.php
+++ src/MVC/application/views/pages/gestionePizzeria/gestionePizzeria.php
@@ -3,13 +3,12 @@
     <br>
     <h3>Gestisci Utenti</h3>
     <hr>
-    <form class="text-center">
+    <div class="text-center">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-6">
                     <div class="row">
-                        <input type="text" class="form-control col-md-8" placeholder="Cerca Utente">
-                        <button class="btn btn-danger col-sm-3" type="submit"><i class="fas fa-search"></i> Cerca</button>
+                        <input type="text" class="form-control col-md-12" placeholder="Cerca Utente" id="cercaUtente">
                     </div>
                 </div>
                 <div class="col-sm-6">
@@ -22,37 +21,37 @@
             </div>
         </div>
         <br>
-        <table class="table table-striped">
-            <thead>
-            <tr>
-                <th scope="col">Username</th>
-                <th scope="col">Tipologia</th>
-                <th scope="col">Modifica</th>
-            </tr>
-            </thead>
-            <tbody>
-                <?php if(isset($_SESSION['utenti']) && count($_SESSION['utenti']) > 0): ?>
-                    <?php $utenti = $_SESSION['utenti']; ?>
-                    <?php foreach ($utenti as $utente): ?>
-                        <?php echo "<tr><td>" . $utente['username'] . "</td><td>" . $utente['tipoUtente'] . "</td><td><a href='" . URL . "gestionePizzeria/modifyUser/" . $utente['username'] . "' class='text-dark'><i class=\"fas fa-user-edit\"></i></a></td></tr>" ?>
-                    <?php endforeach; ?>
-                <?php else: ?>
-                    <?php echo "<tr><td colspan='3'>Nessun utente trovato nel DB.</td>"; ?>
-                <?php endif; ?>
-            </tbody>
-        </table>
-
-    </form>
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">Username</th>
+                    <th scope="col">Tipologia</th>
+                    <th scope="col">Modifica</th>
+                </tr>
+                </thead>
+                <tbody id="utentiTable">
+                    <?php if(isset($_SESSION['utenti']) && count($_SESSION['utenti']) > 0): ?>
+                        <?php $utenti = $_SESSION['utenti']; ?>
+                        <?php foreach ($utenti as $utente): ?>
+                            <?php echo "<tr><td>" . $utente['username'] . "</td><td>" . $utente['tipoUtente'] . "</td><td><a href='" . URL . "gestionePizzeria/modifyUser/" . $utente['username'] . "' class='text-dark'><i class=\"fas fa-user-edit\"></i></a></td></tr>" ?>
+                        <?php endforeach; ?>
+                    <?php else: ?>
+                        <?php echo "<tr><td colspan='3'>Nessun utente trovato nel DB.</td>"; ?>
+                    <?php endif; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
     <br>
     <h3>Gestisci Articoli</h3>
     <hr>
-    <form class="text-center">
+    <div class="text-center">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-6">
                     <div class="row">
-                        <input type="text" class="form-control col-md-8" placeholder="Cerca Articolo">
-                        <button class="btn btn-danger col-sm-3" type="submit"><i class="fas fa-search"></i> Cerca</button>
+                        <input type="text" class="form-control col-md-12" placeholder="Cerca Articolo" id="cercaArticolo"><br>
                     </div>
                 </div>
                 <div class="col-sm-6">
@@ -65,29 +64,52 @@
             </div>
         </div>
         <br>
-        <table class="table table-striped">
-            <thead>
-            <tr>
-                <th scope="col">Immagine</th>
-                <th scope="col">Nome</th>
-                <th scope="col">Prezzo</th>
-                <th scope="col">Modifica</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php if(isset($_SESSION['prodotti']) && count($_SESSION['prodotti']) > 0): ?>
-                <?php $prodotti = $_SESSION['prodotti']; ?>
-                <?php foreach ($prodotti as $prodotto): ?>
-                    <?php echo "<tr><td><img style='height: 50px; width: 50px;' src='" . URL . $prodotto['urlFoto']
-                        . "'></td><td>" . $prodotto['nome'] . "</td><td>" . $prodotto['prezzo'] . "</td><td><a href='" . URL . "gestionePizzeria/modifyArticolo/" . $prodotto['id'] . "' class='text-dark'><i class=\"fas fa-user-edit\"></i></a></td></tr>" ?>
-                <?php endforeach; ?>
-            <?php else: ?>
-                <?php echo "<tr><td colspan='4'>Nessun prodotto trovato nel DB.</td>"; ?>
-            <?php endif; ?>
-            </tbody>
-        </table>
-
-    </form>
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">Immagine</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Prezzo</th>
+                    <th scope="col">Modifica</th>
+                </tr>
+                </thead>
+                <tbody id="articoliTable">
+                <?php if(isset($_SESSION['prodotti']) && count($_SESSION['prodotti']) > 0): ?>
+                    <?php $prodotti = $_SESSION['prodotti']; ?>
+                    <?php foreach ($prodotti as $prodotto): ?>
+                        <?php echo "<tr><td><img style='height: 50px; width: 50px;' src='" . URL . $prodotto['urlFoto']
+                            . "'></td><td>" . $prodotto['nome'] . "</td><td>" . $prodotto['prezzo'] . "</td><td><a href='" . URL . "gestionePizzeria/modifyArticolo/" . $prodotto['id'] . "' class='text-dark'><i class=\"fas fa-user-edit\"></i></a></td></tr>" ?>
+                    <?php endforeach; ?>
+                <?php else: ?>
+                    <?php echo "<tr><td colspan='4'>Nessun prodotto trovato nel DB.</td>"; ?>
+                <?php endif; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
+<script>
+
+    //Script che si occupa della ricerca nelle due tabelle.
+
+    $(document).ready(function(){
+        //Tabella Utente
+        $("#cercaUtente").on("keyup", function() {
+            var value = $(this).val().toLowerCase();
+            $("#utentiTable tr").filter(function() {
+                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            });
+        });
+
+        //Tabella Articolo
+        $("#cercaArticolo").on("keyup", function() {
+            var value = $(this).val().toLowerCase();
+            $("#articoliTable tr").filter(function() {
+                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            });
+        });
+    });
+</script>
 
 
