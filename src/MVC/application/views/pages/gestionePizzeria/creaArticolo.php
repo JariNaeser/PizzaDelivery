4015b9924da74a6f1b31928dbf867e0d69941d9b
Jari Näser <jari.naeser@samtrevano.ch>
2019-10-10 14:05:43 +0200

    Create creaArticolo.php

---
+++ src/MVC/application/views/pages/gestionePizzeria/creaArticolo.php
@@ -0,0 +1,32 @@
+<div class="col-md-12 container text-center padding-footer">
+    <h1>Aggiungi Articolo</h1>
+    <br>
+    <form action="<?php echo URL . "gestionePizzeria/createArticolo" ?>" method="post">
+        <div class="table-responsive col-md-6 mx-auto">
+            <table class="table table-striped">
+                <tbody>
+                    <tr class="text-inline">
+                        <td scope="col">Nome<span class="text-danger"> *</span></td>
+                        <td><input type="text" class="form-control" name="nomeNA" required></td>
+                    </tr>
+                    <tr class="text-inline">
+                        <td scope="col">Descrizione<span class="text-danger"> *</span></td>
+                        <td><input type="text" class="form-control" name="descrizioneNA" required></td>
+                    </tr>
+                    <tr class="text-inline">
+                        <td scope="col">Prezzo<span class="text-danger"> *</span></td>
+                        <td><input type="number" class="form-control" name="prezzoNA" required></td>
+                    </tr>
+                    <tr class="text-inline">
+                        <td scope="col">Nome immagine</td>
+                        <td><input type="text" class="form-control" name="nomeImmaginaNA" placeholder="ES: immagine.png"></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <button type="submit" class="btn btn-danger btn-lg">Aggiungi</button>
+        <a href="<?php echo URL . "gestionePizzeria/home";?>" class="btn btn-danger btn-lg">Esci</a>
+    </form>
+</div>
+
+
