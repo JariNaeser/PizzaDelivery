9c2cad7c3fa057d1a11852a6c334cd4754287966
Jari Näser <jari.naeser@samtrevano.ch>
2019-10-10 15:16:47 +0200

    Update gestionePizzeria.php

--- src/MVC/application/controller/gestionePizzeria.php
+++ src/MVC/application/controller/gestionePizzeria.php
@@ -40,13 +40,6 @@ class GestionePizzeria
         require 'application/views/_templates/footer.php';
     }
 
-    public function creaUtente(){
-        // Carico Views
-        $this->getRightHeader();
-        require 'application/views/pages/gestionePizzeria/creaUser.php';
-        require 'application/views/_templates/footer.php';
-    }
-
     public function modifyUserContent(){
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
             if(isset($_POST['nomeMU'])&&isset($_POST['cognomeMU'])&&isset($_POST['viaMU'])&&isset($_POST['capMU'])&&isset($_POST['paeseMU'])&&isset($_POST['emailMU'])&&isset($_POST['tipologiaMU'])){
@@ -75,16 +68,58 @@ class GestionePizzeria
         $this->home();
     }
 
+    public function creaUtente(){
+        // Carico Views
+        $this->getRightHeader();
+        require 'application/views/pages/gestionePizzeria/creaUser.php';
+        require 'application/views/_templates/footer.php';
+    }
+
     public function createUser(){
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
             if(isset($_POST['nomeNU'])&&isset($_POST['cognomeNU'])&&isset($_POST['viaNU'])&&isset($_POST['capNU'])&&isset($_POST['paeseNU'])&&isset($_POST['emailNU'])&&isset($_POST['passwordNU'])&&isset($_POST['tipologiaNU'])){
                 $this->pdModel->insertQuery("INSERT INTO utente VALUES ('" . strtolower($_POST['nomeNU']) . "." . strtolower($_POST['cognomeNU']) . "', '" . $_POST['nomeNU'] ."', '" . $_POST['cognomeNU'] . "', '" . $_POST['viaNU'] . "', '" . $_POST['capNU'] . "', '" . $_POST['paeseNU'] . "', '" . $_POST['emailNU'] . "', '" . $_POST['passwordNU'] . "', '" . $_POST['tipologiaNU'] . "');");
+                header("Location: " . URL . "gestionePizzeria/home");
                 $this->home();
             }
         }
     }
 
     /* ARTICOLI */
+
+    public function modifyArticolo(int $id){
+
+        $_SESSION['articoloToModify'] = $this->pdModel->getArticolo($id);
+
+        // Carico Views
+        $this->getRightHeader();
+        require 'application/views/pages/gestionePizzeria/editArticolo.php';
+        require 'application/views/_templates/footer.php';
+    }
+
+    public function modifyArticoloContent(){
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            if(isset($_POST['nomeMA'])&&isset($_POST['descrizioneMA'])&&isset($_POST['prezzoMA'])&&isset($_SESSION['articoloToModify'])){
+
+                $nome = $_POST['nomeMA'];
+                $descrizione = $_POST['descrizioneMA'];
+                $prezzo = $_POST['prezzoMA'];
+
+                if(isset($_POST['pathImmaginaMA']) && strlen($_POST['pathImmaginaMA']) > 0){
+                    $path = $_POST['pathImmaginaMA'];
+                    $this->pdModel->insertQuery(  "UPDATE articolo SET nome='$nome', descrizione='$descrizione', prezzo=$prezzo, urlFoto='$path' WHERE id = " . $_SESSION['articoloToModify'][0]['id'] . ";");
+                }else{
+                    $this->pdModel->insertQuery(  "UPDATE articolo SET nome='$nome', descrizione='$descrizione', prezzo=$prezzo WHERE id = " . $_SESSION['articoloToModify'][0]['id'] . ";");
+                }
+                $this->home();
+            }
+        }
+    }
+
+    public function eliminaArticolo(int $id){
+        $this->pdModel->dropArticolo($id);
+        $this->home();
+    }
 
     public function creaArticolo(){
         // Carico Views
@@ -93,12 +128,18 @@ class GestionePizzeria
         require 'application/views/_templates/footer.php';
     }
 
-    public function modifyArticolo(int $id){
-
-    }
-
     public function createArticolo(){
-
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            if(isset($_POST['nomeNA'])&&isset($_POST['descrizioneNA'])&&isset($_POST['prezzoNA'])){
+                if(isset($_POST['nomeImmaginaNA']) && strlen($_POST['nomeImmaginaNA']) > 0){
+                    $this->pdModel->insertQuery("INSERT INTO Articolo(nome, descrizione, prezzo, urlFoto) VALUES ('" . $_POST['nomeNA'] . "', '" . $_POST['descrizioneNA'] ."'," . $_POST['prezzoNA'] . ", '" . $_POST['nomeImmaginaNA'] . "');");
+                }else{
+                    $this->pdModel->insertQuery("INSERT INTO Articolo(nome, descrizione, prezzo) VALUES ('" . $_POST['nomeNA'] . "', '" . $_POST['descrizioneNA'] ."'," . $_POST['prezzoNA'] .");");
+                }
+                header("Location: " . URL . "gestionePizzeria/home");
+                $this->home();
+            }
+        }
     }
 
     private function getRightHeader(){
