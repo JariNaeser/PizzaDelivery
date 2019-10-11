2c7eeb431f8174cde900050207f22c09d4f5b410
Jari Näser <jari.naeser@samtrevano.ch>
2019-10-11 15:41:43 +0200

    move login method

--- src/MVC/application/controller/login.php
+++ src/MVC/application/controller/login.php
@@ -28,7 +28,37 @@ class Login
     }
 
     public function login(){
-        $this->pdModel->login();
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            if(isset($_POST['username']) && isset($_POST['password'])){
+                $username = htmlspecialchars(stripslashes($_POST['username']));
+                $password = htmlspecialchars(stripslashes($_POST['password']));
+                $utenti = $this->getUtenti();
+                $user = null;
+
+                foreach ($utenti as $utente){
+                    if(strcmp($utente['username'], $username) == 0){
+                        $user = $utente;
+                        break;
+                    }
+                }
+
+                if(isset($user) && strcmp($user['password'], $password) == 0){
+                    $_SESSION['user'] = $user;
+
+                    $this->header->getRightHeader();
+                    require 'application/views/pages/index/benvenuto.php';
+                    require 'application/views/_templates/footer.php';
+                }else{
+                    $this->loginForm();
+                    echo "<div class=\"alert alert-warning alert-danger fade show padding-footer\" style='margin: 1em;' role=\"alert\">".
+                        "<strong>Errore:</strong> Il nome utente oppure la password sono errati.".
+                        "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">".
+                        "<span aria-hidden=\"true\">&times;</span>".
+                        "</button>".
+                        "</div>";
+                }
+            }
+        }
     }
 
     public function logout(){
--- src/MVC/application/models/pizzadeliverymodel.php
+++ src/MVC/application/models/pizzadeliverymodel.php
@@ -114,40 +114,6 @@ class PizzaDeliveryModel
         return $this->insertQuery("DELETE FROM utente WHERE username = '$username';");
     }
 
-    public function login(){
-        if($_SERVER['REQUEST_METHOD'] == 'POST'){
-            if(isset($_POST['username']) && isset($_POST['password'])){
-                $username = htmlspecialchars(stripslashes($_POST['username']));
-                $password = htmlspecialchars(stripslashes($_POST['password']));
-                $utenti = $this->pdModel->getUtenti();
-                $user = null;
-
-                foreach ($utenti as $utente){
-                    if(strcmp($utente['username'], $username) == 0){
-                        $user = $utente;
-                        break;
-                    }
-                }
-
-                if(isset($user) && strcmp($user['password'], $password) == 0){
-                    $_SESSION['user'] = $user;
-
-                    $this->header->getRightHeader();
-                    require 'application/views/pages/index/benvenuto.php';
-                    require 'application/views/_templates/footer.php';
-                }else{
-                    $this->loginForm();
-                    echo "<div class=\"alert alert-warning alert-danger fade show padding-footer\" style='margin: 1em;' role=\"alert\">".
-                            "<strong>Errore:</strong> Il nome utente oppure la password sono errati.".
-                            "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">".
-                                "<span aria-hidden=\"true\">&times;</span>".
-                            "</button>".
-                          "</div>";
-                }
-            }
-        }
-    }
-
     public function addToCart(int $id){
         if(isset($_SESSION['cart']) && is_int($id)){
             $item = $this->pdModel->getArticolo($id);
