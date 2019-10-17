a34bbf2d4105250533dd0144c6cdb31a6731e845
Jari Näser <jari.naeser@samtrevano.ch>
2019-10-17 15:31:46 +0200

    Update pizzadeliverymodel.php

--- src/MVC/application/models/pizzadeliverymodel.php
+++ src/MVC/application/models/pizzadeliverymodel.php
@@ -109,28 +109,32 @@ class PizzaDeliveryModel
     public function getFattorini(){
         $fattorini = $this->execQuery("SELECT * FROM Fattorino;");
         for($i = 0; $i < count($fattorini); $i++){
-            $fattorini[$i]['consegneOggi'] = $this->getConsegneOggi($fattorini[$i]['username'])[0]['COUNT(*)'];
+            $fattorini[$i]['consegneOggi'] = $this->getConsegneOggi($fattorini[$i]['username'])[0]['consegneOggi'];
         }
         return $fattorini;
     }
 
     public function getConsegneOggi(string $username){
-        return $this->execQuery("SELECT COUNT(*) FROM Consegna WHERE fattorino LIKE '$username' AND CURRENT_DATE() LIKE CONCAT(YEAR(data), '-', MONTH(data), '-', DAY(data));");
+        return $this->execQuery("SELECT COUNT(*) AS 'consegneOggi' FROM Consegna WHERE fattorino LIKE '$username' AND tipoConsegna LIKE 'terminata' AND CURRENT_DATE() LIKE CONCAT(YEAR(data), '-', MONTH(data), '-', DAY(data));");
     }
 
     public function getFattorino(string $username){
         $fattorino = $this->execQuery("SELECT * FROM Fattorino WHERE username LIKE '$username';");
-        $fattorino['consegneOggi'] = $this->getConsegneOggi($username)[0]['COUNT(*)'];
+        $fattorino['consegneOggi'] = $this->getConsegneOggi($username)[0]['consegneOggi'];
         return $fattorino;
     }
 
     public function getConsegne(string $username){
-        $consegne = $this->execQuery("SELECT * FROM Consegna WHERE fattorino LIKE '$username'");
+        $consegne = $this->execQuery("SELECT * FROM Consegna WHERE fattorino LIKE '$username' AND CONCAT(YEAR(data),'-', MONTH(data),'-', DAY(data)) LIKE CURRENT_DATE()");
         for($i = 0; $i < count($consegne); $i++){
             //Get via
-            $consegne[$i]['via'] = $this->execQuery("SELECT via FROM Ordinazione INNER JOIN Consegna ON Ordinazione.id=Consegna.ordinazione;");
+            $consegne[$i]['via'] = $this->execQuery("SELECT via FROM Ordinazione WHERE id = (SELECT ordinazione FROM Consegna WHERE id = " . $consegne[$i]['id'] . ");");
             //Get totalCost
-            $consegne[$i]['costoTotale'] = //Fare query con 4 join per trovare il prezzo.
+            $consegne[$i]['costoTotale'] = $this->execQuery("SELECT SUM((SELECT prezzo FROM Articolo WHERE id = articolo) * quantita) AS 'SommaCosti'
+                                                                    FROM OrdineArticolo o WHERE ordinazione = (
+                                                                    SELECT ordinazione FROM Consegna c, Fattorino f
+                                                                    WHERE c.fattorino LIKE f.username AND tipoConsegna LIKE 'terminata' AND id = " . $consegne[$i]['id'] . ");
+                                                           ");
         }
         return $consegne;
     }
