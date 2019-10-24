7b954af3ae01df7f59a67ccc051b4783374be708
Jari Näser <jari.naeser@samtrevano.ch>
2019-10-24 14:37:02 +0200

    Update TestDataTabelle.sql

--- src/DB/TestDataTabelle.sql
+++ src/DB/TestDataTabelle.sql
@@ -28,27 +28,6 @@ INSERT INTO Utente VALUES (
 	"amministratore"
 );
 
-INSERT INTO Utente VALUES (
-"paolo.naeser", "paolo", "naeser", 
-"Via Mer Zarei 12", 6965, "Svizzera",
-"paolo.naeser@samtrevano.ch", "test", "fattorino"
-);
-
-/* FATTORINO */
-INSERT INTO Fattorino VALUES (
-	"paolo.naeser", "2.00000000", "34.0203929", true
-);
-
-/* CONSEGNA */
-INSERT INTO `Consegna` (`id`,`dataInserimento`,`dataConsegna`,`tipoConsegna`,`fattorino`,`ordinazione`) VALUES (1,'2019-10-16 14:04:42',NULL,'da effettuare','paolo.naeser', 49);
-INSERT INTO `Consegna` (`id`,`dataInserimento`,`dataConsegna`,`tipoConsegna`,`fattorino`,`ordinazione`) VALUES (2,'2019-09-18 14:06:25',NULL,'in corso','franco.fattorino', 50);
-INSERT INTO `Consegna` (`id`,`dataInserimento`,`dataConsegna`,`tipoConsegna`,`fattorino`,`ordinazione`) VALUES (3,'2019-10-12 14:06:25',NULL,'da effettuare','franco.fattorino', 51);
-INSERT INTO `Consegna` (`id`,`dataInserimento`,`dataConsegna`,`tipoConsegna`,`fattorino`,`ordinazione`) VALUES (4,'2019-03-18 14:06:25',NULL,'in corso','paolo.naeser', 68);
-INSERT INTO `Consegna` (`id`,`dataInserimento`,`dataConsegna`,`tipoConsegna`,`fattorino`,`ordinazione`) VALUES (7,'2018-12-18 14:07:22','2019-10-18 14:07:22','Terminata','franco.fattorino', 69);
-INSERT INTO `Consegna` (`id`,`dataInserimento`,`dataConsegna`,`tipoConsegna`,`fattorino`,`ordinazione`) VALUES (8,'2013-06-18 14:07:34','2019-10-18 14:07:34','Terminata','franco.fattorino', 70);
-INSERT INTO `Consegna` (`id`,`dataInserimento`,`dataConsegna`,`tipoConsegna`,`fattorino`,`ordinazione`) VALUES (9,'2019-10-18 14:07:34','2019-10-18 14:07:34','Terminata','paolo.naeser', 71);
-
-
 /* ARTICOLO */
 INSERT INTO Articolo(nome, descrizione, prezzo, urlFoto) VALUES ("Pizza margherita", "Molto Buona e grande", 12, "application/img/pizzaMargherita.jpg");
 INSERT INTO Articolo(nome, descrizione, prezzo, urlFoto) VALUES ("Pizza ai funghi", "Buona", 13.50, "application/img/pizzaFunghi.jpg");
@@ -58,20 +37,3 @@ INSERT INTO Articolo(nome, descrizione, prezzo, urlFoto) VALUES ("Pizza alla mar
 INSERT INTO Articolo(nome, descrizione, prezzo, urlFoto) VALUES ("Pizza quattro formaggi", "Molto Buona", 14, "application/img/pizzaFormaggi.jpg");
 INSERT INTO Articolo(nome, descrizione, prezzo, urlFoto) VALUES ("Focaccia", "Molto Buona",6, "application/img/focaccia.jpg");
 
-/* ORDINAZIONE */
-INSERT INTO Ordinazione(nomeCliente, cognomeCliente, numeroTelefonoCliente, via, cap, paese, data) VALUES (
-	"Paolo", "Naeser", 0041791223344, "Via Marzo 12", 6900, "Svizzera", now()
-);
-
-/* ORDINE ARTICOLO */
-INSERT INTO OrdineArticolo VALUES (
-	1, 2, 3
-);
-
-
-
-
-
-
-
-/* ALTER TABLE Articolo ALTER urlFoto SET DEFAULT "application/img/defaultPizza.png"; */
