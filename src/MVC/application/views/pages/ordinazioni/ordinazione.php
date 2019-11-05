0fbd5cce136f723686a2fd955a11c1191587afb3
Jari Näser <jari.naeser@samtrevano.ch>
2019-11-05 17:35:28 +0100

    Update map

--- src/MVC/application/views/pages/ordinazioni/ordinazione.php
+++ src/MVC/application/views/pages/ordinazioni/ordinazione.php
@@ -22,9 +22,7 @@
                     </tr>
                 </tbody>
             </table>
-            <div class="col-md-6 bg-dark">
-                <p class="text-white">Google maps</p>
-            </div>
+            <div class="col-md-6" id='map' style="min-height: 200px;"></div>
         </div>
         <br>
         <br>
@@ -60,3 +58,148 @@
 <?php else: ?>
     <?php echo "Session articoli or/and cart not found."; ?>
 <?php endif; ?>
+
+<script>
+
+    var request = "https://api.mapbox.com/geocoding/v5/mapbox.places/<?php echo $ordine[0][0]['via']; ?>.json?limit=1&access_token=pk.eyJ1IjoiamFyaS1uYWVzZXIiLCJhIjoiY2sybHc4YjliMGFsbTNvcDBoNnJvZXludCJ9.b_eEj1vnKie7ZjsR4wNqdA";
+    var requestResponse = null;
+
+    //Make request
+    $.getJSON(request, function(data){
+        console.log(data);
+        var lat = data.features.0.geometry.coordinates.0;
+        //var lon = data[features][0][geometry][coordinates][1];
+        console.log(lat + " " + lon);
+    });
+
+
+
+
+
+
+    //Map
+
+    //Pointer size
+    var size = 150;
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoiamFyaS1uYWVzZXIiLCJhIjoiY2sybHc4YjliMGFsbTNvcDBoNnJvZXludCJ9.b_eEj1vnKie7ZjsR4wNqdA';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [0, 0],
+        zoom: 10
+    });
+
+    var pulsingDot = {
+        width: size,
+        height: size,
+        data: new Uint8Array(size * size * 4),
+
+        onAdd: function() {
+            var canvas = document.createElement('canvas');
+            canvas.width = this.width;
+            canvas.height = this.height;
+            this.context = canvas.getContext('2d');
+        },
+
+        render: function() {
+            var duration = 1000;
+            var t = (performance.now() % duration) / duration;
+
+            var radius = size / 2 * 0.3;
+            var outerRadius = size / 2 * 0.7 * t + radius;
+            var context = this.context;
+
+            // draw outer circle
+            context.clearRect(0, 0, this.width, this.height);
+            context.beginPath();
+            context.arc(this.width / 2, this.height / 2, outerRadius, 0, Math.PI * 2);
+            context.fillStyle = 'rgba(255, 200, 200,' + (1 - t) + ')';
+            context.fill();
+
+            // draw inner circle
+            context.beginPath();
+            context.arc(this.width / 2, this.height / 2, radius, 0, Math.PI * 2);
+            context.fillStyle = 'rgba(255, 100, 100, 1)';
+            context.strokeStyle = 'white';
+            context.lineWidth = 2 + 4 * (1 - t);
+            context.fill();
+            context.stroke();
+
+            // update this image's data with data from the canvas
+            this.data = context.getImageData(0, 0, this.width, this.height).data;
+
+            // keep the map repainting
+            map.triggerRepaint();
+
+            // return `true` to let the map know that the image was updated
+            return true;
+        }
+    };
+
+    map.on('load', function () {
+
+        map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
+
+        map.addLayer({
+            "id": "places",
+            "type": "symbol",
+            "source": {
+                "type": "geojson",
+                "data": {
+                    "type": "FeatureCollection",
+                    "features": [{
+                        "type": "Feature",
+                        "properties": {
+                            "description": "<strong>Fattorino: <?php echo ""; ?></strong><p>Posizione attuale [<?php echo "0"; ?> , <?php echo "0"; ?>]</p>",
+                            "icon": "theatre"
+                        },
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [<?php echo "0"; ?>, <?php echo "0"; ?>]
+                        }
+                    }]
+                }
+            },
+            "layout": {
+                "icon-image": "pulsing-dot",
+                "icon-allow-overlap": true
+            }
+        });
+    });
+
+    //Add popup actions
+
+    // Create a popup, but don't add it to the map yet.
+    var popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false
+    });
+
+    map.on('mouseenter', 'places', function(e) {
+        // Change the cursor style as a UI indicator.
+        map.getCanvas().style.cursor = 'pointer';
+
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var description = e.features[0].properties.description;
+
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        // Populate the popup and set its coordinates
+        // based on the feature found.
+        popup.setLngLat(coordinates)
+            .setHTML(description)
+            .addTo(map);
+    });
+
+    map.on('mouseleave', 'places', function() {
+        map.getCanvas().style.cursor = '';
+        popup.remove();
+    });
+
+</script>
