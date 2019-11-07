ee70478f283a2c804ae65b4bd6a4679c13efb689
Jari Näser <jari.naeser@samtrevano.ch>
2019-11-07 15:06:54 +0100

    Remove bg images

--- src/MVC/application/views/pages/index/benvenuto.php
+++ src/MVC/application/views/pages/index/benvenuto.php
@@ -1,9 +1,7 @@
-<div class="background" style="background: url(<?php echo URL . "application/img/i7.jpg"; ?>) no-repeat center center fixed; -webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;">
-    <div class="col-md-12 welcomeHolder">
-        <h1>Benvenuto da <br> <span class="text-danger">PizzaDelivery</span></h1>
-        <p>Hai fame? Ecco la soluzione per te, ordina e ricevi in <br> breve tempo la tua pizza preferita.</p>
-        <a href="<?php echo URL?>ordina/home" class="btn btn-danger btn-lg">Ordina ora</a>
-    </div>
+<div class="col-md-12 welcomeHolder">
+    <h1>Benvenuto da <br> <span class="text-danger">PizzaDelivery</span></h1>
+    <p>Hai fame? Ecco la soluzione per te, ordina e ricevi in <br> breve tempo la tua pizza preferita.</p>
+    <a href="<?php echo URL?>ordina/home" class="btn btn-danger btn-lg">Ordina ora</a>
 </div>
 
 
