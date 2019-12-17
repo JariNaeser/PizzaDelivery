b1f8c42607b13f4f508da90629516e628705a073
Jari Näser <jari.naeser@samtrevano.ch>
2019-12-17 16:25:25 +0100

    Create modifyLastAdmin.php

---
+++ src/PizzaDelivery/application/views/error/modifyLastAdmin.php
@@ -0,0 +1,5 @@
+<div class="col-md-12 text-center centerVerticallyDiv">
+    <h1 class="text-danger">ERRORE</h1>
+    <h3>Dev'esserci almeno un'admin nel sistema.</h3>
+    <a href="<?php echo URL?>gestionePizzeria/home" class="btn btn-danger btn-lg">Torna agli utenti</a>
+</div>
